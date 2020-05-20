e2e186b32988b4349e630d5a7f698be4336b2b71
mickeyminion14 <sarthakagrawal6@gmail.com>
2020-05-20 20:33:50 +0530

    changes

--- src/app/api/rooms/rooms.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-
-export interface IRoom {
-  roomName: string;
-}
-
-export interface IRoomDocument extends IRoom, Document {}
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from "express";
 import { accountRoutes } from "./app/api/account/account.routes";
-import socket, { Socket } from "socket.io";
-import { roomRoutes } from "./app/api/rooms/rooms.route";
 import mongoose from "mongoose";
 import http from "http";
 var cors = require("cors");
@@ -16,7 +14,6 @@ app.use(cors());
 const PORT = process.env.PORT || 8080;
 
 app.use("/account", accountRoutes);
-app.use("/rooms", roomRoutes);
 
 app.use((req: Request, res: Response) => {
   res.status(404).json({
@@ -33,21 +30,3 @@ mongoose.connect(
     });
   }
 );
-
-const server = http.createServer(app);
-const io = socket.listen(server);
-
-io.sockets.on("connection", (socket: Socket) => {
-  console.log("Connected " + socket.id);
-
-  socket.on("join", function (data: any) {
-    socket.join(data.room);
-    console.log("new user joined" + data.room);
-    socket.broadcast.to(data.room).emit("new user joined", data);
-  });
-
-  socket.on("new-message", (message) => {
-    console.log(message);
-    io.emit(message);
-  });
-});
